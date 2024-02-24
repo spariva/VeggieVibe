4d644a51b91834b519d800105e6553ca8134e604
Maki <makspariva@gmail.com>
2024-02-24 08:24:38 +0100

    .

--- vego/recipes/urls.py
+++ vego/recipes/urls.py
@@ -5,7 +5,6 @@ from django.conf.urls.static import static
 
 app_name = "recipes"
 urlpatterns = [
-    path('', views.index, name='index'),
-    path("recipes", views.RecipeListView.as_view(), name="recipes"),
+    path("", views.RecipeListView.as_view(), name="recipes"),
     path("recipe/<int:pk>", views.RecipeDetailView.as_view(), name="recipe_detail"),
 ]
--- vego/recipes/views.py
+++ vego/recipes/views.py
@@ -1,26 +1,37 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
 from recipes.models import Recipe, Tag
 from django.views import generic
-from django.http import HttpResponse
 
+# from django.core.paginator import Paginator
+# from django.shortcuts import render
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the recipes index.")
 
 class RecipeListView(generic.ListView):
     model = Recipe
+    paginate_by = 10
+    context_object_name = "recipes"  # your own name for the list as a template variable, default is object_list and you can use both
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+
+        search_term = self.request.GET.get("search", "")
+        if search_term:
+            queryset = queryset.filter(title__icontains=search_term)
+
+        return queryset
+
 
 class RecipeDetailView(generic.DetailView):
     model = Recipe
-    template_name = 'recipes/recipe_detail.html'
+    template_name = "recipes/recipe_detail.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['tags'] = Tag.objects.all()
+        context["tags"] = Tag.objects.all()
         return context
-    
+
+
 # def index(request):
 #     recipes = Recipe.objects.all()
 #     context = {'recipes': recipes}
-#     return render(request, 'recipes/index.html', context) 
+#     return render(request, 'recipes/index.html', context)
