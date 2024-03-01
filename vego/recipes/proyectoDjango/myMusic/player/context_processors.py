63fa593986e9838f26eac4e229336dcb99c2c31d
Maki <makspariva@gmail.com>
2024-03-01 14:14:42 +0100

    .

--- vego/recipes/proyectoDjango/myMusic/player/context_processors.py
+++
@@ -1,6 +0,0 @@
-from django.contrib.auth.forms import AuthenticationForm
-
-def login_form(request):
-    return {'login_form': AuthenticationForm()}
-
-#con este archivo haremos que se pueda hacer login globalmente desde cualquier pahgina
